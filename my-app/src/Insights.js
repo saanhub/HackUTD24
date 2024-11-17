e0e5abe96a0718fa53438417a6b09044873db5e1
Preethika Somarapu <91421615+pree5103@users.noreply.github.com>
2024-11-16 19:27:59 -0600

    Insight pages, index.html, ice picture

---
+++ my-app/src/Insights.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './Insights.css';
+
+function Insights() {
+  return (
+    <div className="Insights">
+        <p> 
+      <h1>Welcome to my React App Insights Page!</h1>
+      </p>
+    </div>
+  );
+}
+
+export default Insights;
