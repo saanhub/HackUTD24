07940af4e0e2a7bf22eb0f5aedb033ddde5d7386
saanhub <balasaanvi@gmail.com>
2024-11-17 12:27:29 -0600

    Graph files and images

--- my-app/src/Graph.js
+++ my-app/src/Graph.js
@@ -1,6 +1,10 @@
 import "./Graph.css";
 
 import backgroundImage from "./winter.jpg";
+import GasVolumeGraph from "./GasVolumeGraph.png";
+import ErrorGraph from "./ErrorGraph.png";
+import Switch from "./switch.png";
+import chatbot from "./chatbot.png";
 import blueBackground from "./bluebg.png";
 //    //<div className="Insights-hero-background" style={{ background: `url(${backgroundImage}) 50% / cover no-repeat` }}></div>
 import eogWord from "./eog.png";
@@ -16,6 +20,10 @@ function Graph() {
         <h1 className="Small-title-text">LEASE OPERATOR</h1>
       </div>
       <div
+        className="chatbot-image-background"
+        style={{ background: `url(${chatbot}) 50% / cover no-repeat` }}
+      ></div>
+      <div
         className="EOG-image-background"
         style={{ background: `url(${eogWord}) 50% / cover no-repeat` }}
       ></div>
@@ -30,8 +38,39 @@ function Graph() {
       <div className="White-line"></div>
       <div className="App-hero-background-purple"></div>
       <div className="Graph-Purple-container"></div>
+      <div className="Graph2-Purple-container"></div>
       <div className="Graph-container">
-        <h1 className="Graph-text">Prediction Graph</h1>
+        <h1 className="Graph-text">Gas Volume Graph</h1>
+      </div>
+      <div className="Custom-ButtonContainer">
+        <button className="Custom-Button">Need info from another well?</button>
+      </div>
+
+      <div
+        className="Gas-Vol-image-background"
+        style={{ background: `url(${GasVolumeGraph}) 50% / cover no-repeat` }}
+      ></div>
+
+      <div
+        className="Error-image"
+        style={{ background: `url(${ErrorGraph}) 50% / cover no-repeat` }}
+      ></div>
+
+      <div className="Graph2-container">
+        <h1 className="Graph2-text">Set Point Difference</h1>
+      </div>
+
+      <div className="Sol-Purple-container"></div>
+      <div className="Sol2-Purple-container"></div>
+
+      <div className="Sol-text-container ">
+        <h1 className="Sol-text">SOLUTIONS</h1>
+      </div>
+      <div className="Sol1-text-container ">
+        <h1 className="Sol1-text">METHANOL PUMP on/off</h1>
+      </div>
+      <div className="Sol2-text-container ">
+        <h1 className="Sol2-text">TURN OFF VALVE on/off</h1>
       </div>
     </div>
   );
